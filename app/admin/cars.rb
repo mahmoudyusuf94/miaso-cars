8c589e38d19a38adf974b13f1f12e1a9d78764a2
Mahmoud Yusuf <mahmoud.yusuf94@gmail.com>
2018-09-29 14:26:35 +0200

    Add permit parameters

--- app/admin/cars.rb
+++ app/admin/cars.rb
@@ -2,8 +2,8 @@ ActiveAdmin.register Car do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
-#
+permit_params :brand, :model, :plate_no, :assigned
+
 # or
 #
 # permit_params do
